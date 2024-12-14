b0e464ab0e303e9edf0cf9fd25d2a7675572f9b0
Basit Ali <156661884+Basit97@users.noreply.github.com>
2024-12-15 00:02:12 +0530

    Player.hpp

--- main/Player.hpp
+++ main/Player.hpp
@@ -1,14 +1,15 @@
-#pragma once
 #include <string>
+#include "Pokemon.hpp"
 using namespace std;
 
-class Player {
+// Include the new Pokemon headerclass 
+Player{
 public:
     string name;
     Pokemon chosenPokemon;
 
     Player(); // Default constructor
-    Player(std::string p_name, Pokemon p_chosenPokemon); // Parameterized constructor
+    Player(string p_name, Pokemon p_chosenPokemon); // Parameterized constructor
 
     void choosePokemon(int choice); // Method to choose a Pokemon
 };
