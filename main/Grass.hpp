0902f38944d8abb152950ddefa1a931e80fd397c
Basit Ali <156661884+Basit97@users.noreply.github.com>
2024-12-15 23:15:05 +0530

    Grass.hpp

---
+++ main/Grass.hpp
@@ -0,0 +1,11 @@
+#pragma once
+// grass.hpp
+#include<string>
+#include<vector>
+using namespace std;
+
+struct Grass {
+    string environmentType;  // Example: "Forest", "Cave", "Riverbank"
+    vector<Pokemon> wildPokemonList;  // List of wild Pokémon that live in this grass
+    int encounterRate;  // Likelihood of encountering a wild Pokémon, out of 100
+};
