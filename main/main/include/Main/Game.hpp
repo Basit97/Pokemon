51073de9b7372e639ef691838a1a495706163ac1
Basit Ali <156661884+Basit97@users.noreply.github.com>
2025-01-08 23:32:05 +0530

    Game.hpp

--- main/main/include/Main/Game.hpp
+++ main/main/include/Main/Game.hpp
@@ -1,12 +1,22 @@
 #pragma once
-#include "Grass.hpp"
+#include "../Character/Player/Player.hpp"
+#include "../Pokemon/Grass.hpp"
 
-class Player;
+// class Player;
+namespace N_Main
+{
+    using namespace N_Pokemon;
+    using namespace N_Character::N_Player;
 
-class Game {
-private:
-	Grass forestGrass;
-public:
-	Game();
-	void gameLoop(Player& player);
-};
+    class Game {
+    private:
+        Grass forestGrass;
+        Pokemon* wildPokemon;
+
+    public:
+        Game();
+        ~Game();
+        void gameLoop(Player* player);
+        void visitPokeCenter(Player* player);
+    };
+}
