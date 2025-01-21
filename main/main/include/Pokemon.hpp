6d2868f9c85e6525214ed28df2d2263a3b34507a
Basit Ali <156661884+Basit97@users.noreply.github.com>
2025-01-21 23:40:24 +0530

    Update Pokemon.hpp

--- main/main/include/Pokemon.hpp
+++ main/main/include/Pokemon.hpp
@@ -1,23 +1,28 @@
 #pragma once
 #include <string>
-using namespace std;
 
-enum class PokemonType;
-
-class Pokemon {
-public:
-    string name;
-    PokemonType type;
-    int health;
-    int maxHealth;
-    int attackPower;
-
-    Pokemon();
-    Pokemon(string p_name, PokemonType p_type, int p_health, int p_attackPower);
-    Pokemon(const Pokemon& other);
-
-    bool isFainted() const;
-    void heal();
-    void attack(Pokemon& target);
-    void takeDamage(int damage);
-};
+namespace N_Pokemon {
+    
+    using namespace std;
+    
+    enum class PokemonType;
+    
+    class Pokemon {
+    protected:
+        string name;
+        PokemonType type;
+        int health; 
+        int maxHealth; 
+        int attackPower;
+        
+    public:
+        Pokemon(); 
+        Pokemon(string p_name, PokemonType p_type, int p_health, int p_attackPower);
+        Pokemon(const Pokemon &other);
+    
+        bool isFainted() const;
+        void heal();
+        void attack(Pokemon &target);
+        void takeDamage(int damage);
+    };
+}
